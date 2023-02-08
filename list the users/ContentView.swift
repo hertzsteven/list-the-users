9542a9ef98222c400d204c9af96637109aa8198a
Steven Hertz <hertzsteven@icloud.com>
2023-02-08 08:54:16 -0500

    Initial Commit

---
+++ list the users/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  list the users
+//
+//  Created by Steven Hertz on 2/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ list the users/list_the_usersApp.swift
@@ -0,0 +1,17 @@
+//
+//  list_the_usersApp.swift
+//  list the users
+//
+//  Created by Steven Hertz on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct list_the_usersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
